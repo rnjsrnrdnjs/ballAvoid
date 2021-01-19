a441098b2d559525461de1953b56c3d910d105d3
rnjsrnrdnjs <090tree@gmail.com>
2021-01-19 14:58:20 +0000

    two

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,4 @@
-var NBALL = 12; // 공의 개수
+var NBALL = 0; // 공의 개수
 var R; // 공의 반지름
 var BACK_ALPHA = 0.3; // 배경의 α 값
 var canvas;
@@ -8,45 +8,169 @@ var wall;
 var balls = [];
 var myball = {};
 var TIME_INTERVAL = 33;
+var maxHigh = 0;
+var high, low;
+var bP, raf;
+var tof = true;
+//컨트롤바
+var downclick=false;
+function disableScroll() {
+	document.body.classList.add('stop-scrolling');
+}
 window.onload = function () {
 	// 각종 매개변수
+	let stopScroll = disableScroll();
 	canvas = document.getElementById('canvas');
 	ctx = canvas.getContext('2d');
-	R = canvas.width / 100;
+	R = 1;
 	wall = { left: 0, right: canvas.width, top: 0, bottom: canvas.height };
-	for (var i = 0; i < NBALL; i++) {
-		balls[i] = new Ball(wall.right / 2, wall.bottom / 2, R);
-		balls[i].setVelocityAsRandom(0.5, 1).setColorAsRandom(50, 255);
+	high = document.getElementById('high');
+	high.innerHTML = `최고점수 : ${maxHigh}`;
+
+	myball = new Ball(wall.right / 4, wall.bottom / 4, R + 2);
+	myball.setColor(1);
+	myball.collisionWall(wall).draw(ctx);
+
+	var one = document.getElementById('one');
+	one.addEventListener('touchstart', down,false);
+	one.addEventListener('touchmove', move,false);
+	one.addEventListener('touchend', up,false);
+	one.addEventListener('mousedown', down,false);
+	one.addEventListener('mousemove', move,false);
+	one.addEventListener('mouseup', up,false);
+	var ctx5 = one.getContext('2d');
+	ctx5.lineWidth = 2;
+
+	clearBackground();
+	clearBackground2(0, 0);
+	drawCircle(one.width / 2, one.height / 2, one.height / 5, 'rgb(0,255,255)');
+	var startX,
+		startY,
+		moveX = 0,
+		moveY = 0;
+	var joyPos = one.getBoundingClientRect();
+	var onTouch = false;
+	function down(event) {
+		try {
+			startX = Math.round(event.touches[0].clientX - joyPos.left);
+			startY = Math.round(event.touches[0].clientY - joyPos.top);
+		} catch {
+			startX = Math.round(event.clientX - joyPos.left);
+			startY = Math.round(event.clientY - joyPos.top);
+		}
+		onTouch = true;
 	}
-	/*
-	setInterval(drawFrame, TIME_INTERVAL);
-	// 애니메이션의 프레임 그리기
-	function drawFrame() {
-		// 배경을 검은색으로 칠한다
-		ctx.fillStyle = 'rgba(0,0,0,' + BACK_ALPHA + ')';
-		ctx.fillRect(0, 0, canvas.width, canvas.height);
-		// 공의 위치를 ​갱신하여 그린다
-		for (i = 0; i < balls.length; i++) {
-			balls[i].move().collisionWall(wall).draw(ctx);
+	var moveMax = one.height / 4;
+	function move(event) {
+		if (onTouch) {
+			try {
+				moveX = Math.round(event.touches[0].clientX - joyPos.left) - startX;
+				moveY = Math.round(event.touches[0].clientY - joyPos.top) - startY;
+			} catch {
+				moveX = Math.round(event.clientX - joyPos.left) - startX;
+				moveY = Math.round(event.clientY - joyPos.top) - startY;
+			}
+
+			if (moveX > moveMax) moveX = moveMax;
+			else if (moveX < -moveMax) moveX = -moveMax;
+			if (moveY > moveMax) moveY = moveMax;
+			else if (moveY < -moveMax) moveY = -moveMax;
+
+			clearBackground();
+			clearBackground2(moveX, moveY);
+			drawCircle(
+				one.width / 2 + moveX,
+				one.height / 2 + moveY,
+				one.height / 5,
+				'rgb(0,255,255)'
+			);
+			
+			// 공움직이는 이벤트 추가
+			document.addEventListener("onTouch",moveBall,false);
 		}
 	}
-	*/
-	draw();
+	function moveBall(){
+		console.log(1);
+			myball.x+=moveX/40;
+			myball.y+=moveY/40;
+	}
+	function up() {
+		clearBackground();
+		clearBackground2(0, 0);
+		drawCircle(one.width / 2, one.height / 2, one.height / 5, 'rgb(0,255,255)');
+		onTouch = false;
+	}
+	function clearBackground() {
+		ctx5.clearRect(0, 0, one.width, one.height);
+		ctx5.beginPath();
+		ctx5.strokeStyle = 'rgb(0,0,0)';
+		ctx5.arc(one.width / 2, one.height / 2, one.height / 2, 0, 2 * Math.PI);
+		ctx5.stroke();
+	}
+	function clearBackground2(mx, my) {
+		ctx5.beginPath();
+		ctx5.strokeStyle = 'rgb(0,255,255)';
+		ctx5.arc(one.width / 2 + mx, one.height / 2 + my, one.height / 4, 0, 2 * Math.PI);
+		ctx5.stroke();
+	}
+	function drawCircle(x, y, r, c) {
+		ctx5.beginPath();
+		ctx5.fillStyle = c;
+		ctx5.arc(x, y, r, 0, 2 * Math.PI);
+		ctx5.fill();
+	}
+
+	start();
 };
+function start() {
+	bP = setInterval(ballPush, 5000);
+	raf = requestAnimationFrame(draw);
+}
+function end() {
+	console.log(2);
+	clearInterval(bP);
+	maxHigh = Math.max(maxHigh, low);
+	cancelAnimationFrame(raf);
+}
+
 function draw() {
-	
+	//공과 공 맞닿은 처리
+	if (!tof) return;
+	for (i = 0; i < balls.length; i++) {
+		if (
+			Math.sqrt(
+				Math.abs(myball.y - balls[i].y) * Math.abs(myball.y - balls[i].y) +
+					Math.abs(myball.x - balls[i].x) * Math.abs(myball.x - balls[i].x)
+			) <=
+			myball.r + balls[i].r
+		) {
+			end();
+			tof = false;
+			return;
+		}
+	}
 	drawFrame();
 	requestAnimationFrame(draw);
 }
 function drawFrame() {
-		// 배경을 검은색으로 칠한다
-		ctx.fillStyle = 'rgba(0,0,0,' + BACK_ALPHA + ')';
-		ctx.fillRect(0, 0, canvas.width, canvas.height);
-		// 공의 위치를 ​갱신하여 그린다
-		for (i = 0; i < balls.length; i++) {
-			balls[i].move().collisionWall(wall).draw(ctx);
-		}
+	// 배경을 검은색으로 칠한다
+	myball.collisionWall(wall).draw(ctx);
+	ctx.fillStyle = 'rgba(0,0,0,' + BACK_ALPHA + ')';
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	//myball.move().collisionWall(wall).draw(ctx);
+	// 공의 위치를 ​갱신하여 그린다
+	for (i = 0; i < balls.length; i++) {
+		balls[i].move().collisionWall(wall).draw(ctx);
 	}
+	low = document.getElementById('low');
+	low.innerHTML = `현재점수 : ${balls.length}`;
+}
+function ballPush() {
+	var cball = new Ball(wall.right / 2, wall.bottom / 2, R);
+	cball.setVelocityAsRandom(0.2, 0.5).setColor(0);
+	balls.push(cball);
+}
+
 // Ball 생성자
 function Ball(x, y, r, vx, vy, color) {
 	this.x = x; // 공의 x좌표
@@ -67,11 +191,9 @@ Ball.prototype = {
 		return this;
 	},
 	// 색상을 임의로 설정
-	setColorAsRandom: function (lmin, lmax) {
-		var R = Math.floor(lmin + Math.random() * (lmax - lmin));
-		var G = Math.floor(lmin + Math.random() * (lmax - lmin));
-		var B = Math.floor(lmin + Math.random() * (lmax - lmin));
-		this.color = 'rgb(' + R + ',' + G + ',' + B + ')';
+	setColor: function (ch) {
+		if (ch) this.color = 'rgb(' + 0 + ',' + 255 + ',' + 255 + ')';
+		else this.color = 'rgb(' + 255 + ',' + 0 + ',' + 0 + ')';
 		return this;
 	},
 	// 공 그리기
